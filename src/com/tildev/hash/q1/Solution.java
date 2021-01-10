abe584c7693fb793d92f61f45ad23c90aa9f69c7
tildev <tildevelove@gmail.com>
2021-01-10 15:01:43 +0900

    Solve the problem 완주하지못한선수 of hash

---
+++ src/com/tildev/hash/q1/Solution.java
@@ -0,0 +1,31 @@
+package com.tildev.hash.q1;
+
+
+import java.util.Arrays;
+
+/**
+ * 완주하지 못한 선수
+ *
+ * @author tildev
+ * @data 2020. 01. 10.
+ */
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+        Arrays.sort(participant);
+        Arrays.sort(completion);
+        int i;
+        for (i = 0; i < completion.length; i++) {
+            if (!participant[i].equals(completion[i])) {
+                return participant[i];
+            }
+        }
+        return participant[i];
+    }
+
+
+    public static void main(String[] args) {
+        String[] participant = {"leo", "kiki", "eden"};
+        String[] completion = {"kiki", "eden"};
+        new Solution().solution(participant, completion);
+    }
+}
