c6c11b42352235f8ee64f14766f8154971c729eb
tildev <tildevelove@gmail.com>
2021-02-01 21:45:41 +0900

    Add solution - Kth number of sort

--- src/com/tildev/sort/level1/q1/Solution.java
+++ src/com/tildev/sort/level1/q1/Solution.java
@@ -2,6 +2,7 @@ package com.tildev.sort.level1.q1;
 
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
 
@@ -12,6 +13,13 @@ import java.util.List;
  * @data 2021. 02. 01.
  */
 class Solution {
+    /**
+     * using list
+     *
+     * @param array
+     * @param commands
+     * @return
+     */
     public int[] solution(int[] array, int[][] commands) {
         int[] answer = new int[commands.length];
         List<Integer> list;
@@ -27,12 +35,34 @@ class Solution {
         return answer;
     }
 
+    /**
+     * using copyOfRange
+     *
+     * @param array
+     * @param commands
+     * @return
+     */
+    public int[] solution2(int[] array, int[][] commands) {
+        int[] answer = new int[commands.length];
+        for (int i = 0; i < commands.length; i++) {
+            int[] temp = Arrays.copyOfRange(array, commands[i][0] - 1, commands[i][1]);
+            Arrays.sort(temp);
+            answer[i] = temp[commands[i][2] - 1];
+        }
+
+        return answer;
+    }
 
     public static void main(String[] args) {
         int[] array = {1, 5, 2, 6, 3, 7, 4};
         int[][] commands = {{2, 5, 3}, {4, 4, 1}, {1, 7, 3}};
         int[] answer = new Solution().solution(array, commands);
         for (int ans : answer) {
+            System.out.print(ans + " ");
+        }
+        System.out.println();
+        int[] answer2 = new Solution().solution2(array, commands);
+        for (int ans : answer2) {
             System.out.print(ans + " ");
         }
     }
