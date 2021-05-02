e8abbe01f2dd0003e81e6be4709b0a3c2a74168f
tildev <tildevelove@gmail.com>
2021-05-02 12:41:50 +0900

    Solve the problem lv2 42578

---
+++ src/com/tildev/c30/lv2/l42578/Solution.java
@@ -0,0 +1,42 @@
+package com.tildev.c30.lv2.l42578;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 위장
+ * <p>
+ * 해시
+ *
+ * @author tildev
+ * @data 2021. 05. 02.
+ */
+class Solution {
+
+  public int solution(String[][] clothes) {
+    int answer = 1;
+
+    Map<String, Integer> clothesTypeMap = new HashMap<>();
+
+    for (int i = 0; i < clothes.length; i++) {
+      clothesTypeMap.put(clothes[i][1], clothesTypeMap.getOrDefault(clothes[i][1], 0) + 1);
+    }
+
+    for (String key : clothesTypeMap.keySet()) {
+      // 해당 옷 종류를 입지 않았을 경우의 수 +1 을 해준다.
+      answer *= (clothesTypeMap.get(key) + 1);
+    }
+
+    // 아무것도 위장하지 않은 경우의 수 1을 빼준다.
+    return answer - 1;
+  }
+
+  public static void main(String[] args) {
+    String[][] clothes = {{"yellowhat", "headgear"}, {"bluesunglasses", "eyewear"},
+        {"green_turban", "headgear"}};
+
+    int ans = new Solution().solution(clothes);
+    System.out.println(ans);
+
+  }
+}
