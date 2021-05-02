dc8bc97c38a67e246faf1c0a37783d9b26374248
tildev <tildevelove@gmail.com>
2021-05-02 12:20:11 +0900

    Solve the problem - lv2 42747

---
+++ src/com/tildev/c30/lv2/l42747/Solution.java
@@ -0,0 +1,39 @@
+package com.tildev.c30.lv2.l42747;
+
+
+import java.util.Arrays;
+
+/**
+ * H-Index 정렬
+ *
+ * @author tildev
+ * @data 2021. 05. 02.
+ */
+class Solution {
+
+  public int solution(int[] citations) {
+    int answer = 0;
+    Arrays.sort(citations);
+
+    int h;
+    for (int i = 0; i < citations.length; i++) {
+      h = citations.length - i;
+
+      // h번 이상 인용된 논문의 수
+      if (citations[i] >= h) {
+        answer = h;
+        break;
+      }
+    }
+
+    return answer;
+  }
+
+  public static void main(String[] args) {
+    int[] citations = {3, 0, 6, 1, 5};
+    int answer = new Solution().solution(citations);
+
+    System.out.println(answer);
+
+  }
+}
