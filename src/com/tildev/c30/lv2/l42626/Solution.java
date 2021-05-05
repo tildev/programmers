65bb157d80444d16511abecd63924f732e594778
tildev <tildevelove@gmail.com>
2021-05-05 23:38:01 +0900

    Init lv2 42626

---
+++ src/com/tildev/c30/lv2/l42626/Solution.java
@@ -0,0 +1,25 @@
+package com.tildev.c30.lv2.l42626;
+
+/**
+ * 더 맵게
+ * <p>
+ * 힙(Heap)
+ *
+ * @author tildev
+ * @data 2021. 05. 05.
+ */
+class Solution {
+  public int solution(int[] scoville, int K) {
+    int answer = 0;
+    return answer;
+  }
+  
+  public static void main(String[] args) {
+    int[] scoville = {1, 2, 3, 9, 10, 12};
+    int k = 7;
+    int ans = new Solution().solution(scoville, k);
+
+    System.out.println(ans);
+
+  }
+}
