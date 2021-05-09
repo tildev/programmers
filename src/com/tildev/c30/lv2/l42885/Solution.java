2b24f2d85729a5148ab44357d418bb0085480ce0
tildev <tildevelove@gmail.com>
2021-05-09 23:17:00 +0900

    Init lv2 42885

---
+++ src/com/tildev/c30/lv2/l42885/Solution.java
@@ -0,0 +1,27 @@
+package com.tildev.c30.lv2.l42885;
+
+/**
+ * 구명보트
+ * <p>
+ * 탐욕법(Greedy)
+ *
+ * @author tildev
+ * @data 2021. 05. 09.
+ */
+class Solution {
+
+  public int solution(int[] people, int limit) {
+    int answer = 0;
+    return answer;
+  }
+
+  public static void main(String[] args) {
+    int[] people = {70, 50, 80, 50};
+    int limit = 100;
+
+    int ans = new Solution().solution(people, limit);
+
+    System.out.println(ans);
+
+  }
+}
