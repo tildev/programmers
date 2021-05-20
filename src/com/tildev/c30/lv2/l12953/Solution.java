a1529937e0c6e4f197d1847d50d722521a8d163b
tildev <tildevelove@gmail.com>
2021-05-20 18:29:12 +0900

    Solve the problem 12953

---
+++ src/com/tildev/c30/lv2/l12953/Solution.java
@@ -0,0 +1,47 @@
+package com.tildev.c30.lv2.l12953;
+
+/**
+ * N개의 최소공배수
+ * <p>
+ * 연습문제
+ *
+ * @author tildev
+ * @data 2021. 05. 20.
+ */
+public class Solution {
+
+  // 최대공약수
+  private static int getGCD(int a, int b) {
+    while (b > 0) {
+      int temp = b;
+      b = a % b;
+      a = temp;
+    }
+    return a;
+  }
+
+  // 최소공배수
+  public static int getLcm(int a, int b, int gcd) {
+    return (a * b) / gcd;
+  }
+
+  public int solution(int[] arr) {
+    if (arr.length == 1) {
+      return arr[0];
+    }
+    int answer = getLcm(arr[0], arr[1], getGCD(arr[0], arr[1]));
+    for (int i = 2; i < arr.length; i++) {
+      answer = getLcm(answer, arr[i], getGCD(answer, arr[i]));
+    }
+    return answer;
+  }
+
+  public static void main(String[] args) {
+    int[] arr = {2, 6, 8, 14}; // result : 168
+    int[] arr2 = {1, 2, 3}; // result : 6
+
+    int ans = new Solution().solution(arr);
+    System.out.print(ans + " ");
+
+  }
+}
